725143fdac9fb47090797ac43b601b693b7eca14
svyx6ktgqy-prog <svyx6ktgqy@privaterelay.appleid.com>
2026-07-28 13:44:29 -0300

    Actualizar source.lua

--- source.lua
+++ source.lua
@@ -1378,7 +1378,7 @@ task.spawn(function()
 	-------------------------------------------------------
 	local rotationSpeed = 30     -- Velocidad del giro
 	local crossfadeSpeed = 3     -- Velocidad de transición color
-	local flashDuration = 2.3    -- Velocidad del destello
+	local flashDuration = 2.8    -- Velocidad del destello
 	
 	-- Paleta recuperada: Incluye los tramos de sombra negra viajera
 	local baseColors = {
